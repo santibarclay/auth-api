113d20e7feffbe08e338faa8e909b498d08732d8
Santiago Barclay <santibarclay@hotmail.com>
2025-05-04 11:25:41 -0300

    fresh start

--- src/routes/token.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tokenValidatorController = require('../controllers/tokenValidator');
-
-// Ruta para validar tokens
-router.post('/validate', tokenValidatorController.validateToken);
-
-module.exports = router;
